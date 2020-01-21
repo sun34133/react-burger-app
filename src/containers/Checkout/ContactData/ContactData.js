d5022892092539acae27734f09cd2c062dfcdd06
Sunil Kumar <sun34133@gmail.com>
2020-01-21 17:30:20 +0530

    managing guards for routes
    managing guards for routes

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -106,7 +106,8 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingreds,
       price: this.props.totPrice,
-      orderData: formData
+      orderData: formData,
+      userId: this.props.userId
     };
 
     this.props.onOrderBurger(order, this.props.token);
@@ -210,7 +211,8 @@ const mapStateToProps = state => {
     ingreds: state.burgerBuilder.ingredients,
     totPrice: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   }
 };
 
