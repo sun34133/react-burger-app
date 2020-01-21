3c5765c2b3811650766549c217a5b5419a6b775d
Sunil Kumar <sun34133@gmail.com>
2020-01-21 16:53:13 +0530

    manage login state
    manage login state

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,9 @@ export const authFail = (error) => {
 };
 
 export const authLogout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('expirationDate');
     return {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -50,6 +53,11 @@ export const auth = (email, password, isSignUp) => {
         }
         axios.post(actionUrl, authdata)
             .then(response => {
+                const expirationDate = new Date(new Date().getTime() + (response.data.expiresIn * 1000));
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('userId', response.data.localId);
+                localStorage.setItem('expirationDate', expirationDate);
+
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -63,5 +71,23 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    };
+};
+
+export const authCheckStatus = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(authLogout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate <= new Date()) {
+                dispatch(authLogout());
+            } else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/1000));
+            }
+        }
     };
 };
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -14,5 +14,6 @@ export {
 export {
     auth,
     authLogout,
-    setAuthRedirectPath
+    setAuthRedirectPath,
+    authCheckStatus
 } from './auth';
